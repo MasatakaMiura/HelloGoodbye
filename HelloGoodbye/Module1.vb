0c7ff6c7c4fa60066e09dad1bbde5ce11b411014
MasatakaMiura <masataka.m@triplet-rd.com>
2017-04-11 13:16:19 +0900

    PT-3

--- HelloGoodbye/Module1.vb
+++ HelloGoodbye/Module1.vb
@@ -1,7 +1,7 @@
 ﻿Module Module1
 
     Sub Main()
-        MsgBox("Goodbye:)") ' Display message on computer screen.
+        MsgBox("Goodbye??") ' Display message on computer screen.
     End Sub
 
 End Module
